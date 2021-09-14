9c0b3bc7bb6597feed022ab137fe6b2d494fd3a2
Helton Carlos <heltoncarlospa@hotmail.com>
2021-09-13 22:36:17 -0300

    adicionando card

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,127 @@
 <template>
   <div class="q-pa-md">
     <div class="row">
-      <div class="col-4">
+      <div class="col-6">
         <q-table
           title="CHECK DE SERVIÇOS"
           :rows="rows"
           :columns="columns"
           row-key="name"
         />
+        <q-card class="my-card q-ml-md q-mt-md">
+          <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="place"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%)"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">Cafe Basilico</div>
+              <div
+                class="
+                  col-auto
+                  text-grey text-caption
+                  q-pt-md
+                  row
+                  no-wrap
+                  items-center
+                "
+              >
+                <q-icon name="place" />
+                250 ft
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-subtitle1">$・Italian, Cafe</div>
+            <div class="text-caption text-grey">
+              Small plates, salads & sandwiches in an intimate setting.
+            </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions>
+            <q-btn flat round icon="event" />
+            <q-btn flat color="primary"> Reserve </q-btn>
+          </q-card-actions>
+        </q-card>
       </div>
       <div class="col-6">
+        <q-card class="my-card q-ml-md">
+          <q-card-section horizontal>
+            <q-img
+              class="col-5"
+              src="https://cdn.quasar.dev/img/parallax1.jpg"
+            />
+
+            <q-card-section>
+              {{ today }}
+            </q-card-section>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions>
+            <q-btn flat round icon="event" />
+            <q-btn flat> 8:30PM </q-btn>
+            <q-btn flat> 19:00PM </q-btn>
+            <q-btn flat color="primary"> Escalas </q-btn>
+          </q-card-actions>
+        </q-card>
+
+        <q-card class="my-card q-ml-md q-mt-md">
+          <img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+          <q-list>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="primary" name="local_bar" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Bar XYZ</q-item-label>
+                <q-item-label caption>Have a drink.</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="red" name="local_gas_station" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Gas Station</q-item-label>
+                <q-item-label caption>Fill your gas tank.</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon color="amber" name="local_movies" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Cinema XYZ</q-item-label>
+                <q-item-label caption>Watch a movie.</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { ref } from "vue";
 const columns = [
   {
     name: "name",
@@ -80,6 +187,12 @@ export default {
     return {
       columns,
       rows,
+      stars: ref(4),
+    };
+  },
+  data() {
+    return {
+      today: new Date(),
     };
   },
 };
